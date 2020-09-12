5b6f7fc835737defb1bb6dbf438e333fd21c4dd0
jacob-zietek <xoqcpn@gmail.com>
2020-09-12 14:59:31 -0400

    misc? I forgot what I did sorry to anyone whos looking at this

--- templated-interphase/js/main.js
+++ templated-interphase/js/main.js
@@ -6,6 +6,7 @@
 // function call to some method within main.js
 
 var numPeople;
+var coords;
 
 function genSocialDistance(){
     len1ForSim = document.getElementById("length").value;
@@ -14,4 +15,23 @@ function genSocialDistance(){
 
     setup();
     document.getElementById('drawingCanvas').style.display='block'; // This shows the HTML5 canvas
+
+    var maxPeople = Math.ceil(len1ForSim/6) * Math.ceil(len2ForSim/6);
+    console.log("Max people: " + maxPeople);
+
+    //getUserLocation();    
+}
+
+function getUserLocation(){
+    navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position.coords.latitude);
+          console.log(position.coords.longitude);
+          coords = position.coords
+        },
+        error => {
+          console.log(error.message);
+        },
+        {timeout:10000, enableHighAccuracy:true, maximumAge:60000}
+     )
 }
